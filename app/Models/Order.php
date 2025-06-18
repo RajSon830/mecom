53254c501e8abc509fdc205f26b80ebf08a31d0a
RajSon830 <rajsonawala2023@gmail.com>
2025-06-18 17:12:34 +0200

    first commit

---
+++ app/Models/Order.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Order extends Model
+{
+    //
+
+    protected $fillable = [
+        'product_id',
+        'user_id',
+        'status',
+        'quantity',
+        'price_per_item',
+        'total_price'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
